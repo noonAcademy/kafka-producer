dcc38931559589e197adc8084b5240cf374d5f4c
Akshit <akin.park@gmail.com>
2019-05-20 14:39:11 +0530

    Logic to produce data in noon-gate - Logic to trigger noon-gate API - Making the HTTP parameters as configurable - Exposing it to be used in the code bases

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const initConsumer = require('./consumer').initConsumer;
 const initProducer = require('./producer').initProducer;
+const sendRecordToNoonGate = require('./noon_gate_producer')
+  .sendRecordToNoonGate;
 
 exports.initConsumer = initConsumer;
 exports.initProducer = initProducer;
+exports.sendRecordToNoonGate = sendRecordToNoonGate;
---
+++ noon_gate_producer.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const uuid = require('uuid');
+const noonGateProducerConstants = require('./lib/constants/noon_gate_producer');
+
+const noonGateAddress = `${noonGateProducerConstants.noonGateRpcHostConstant}:${
+  noonGateProducerConstants.noonGateRpcPortConstant
+}`;
+
+sendRecordToNoonGate = async ({ type, userId, data }, topic) => {
+  let response;
+  const url = `${noonGateAddress}/${
+    noonGateProducerConstants.noonGateRouteConstant
+  }`;
+  const bodyData = {
+    id: uuid.v4(),
+    timestamp: Date.now(),
+    userId: userId,
+    type: type,
+    data: data,
+    topic: topic
+  };
+  try {
+    response = await axios.post(url, bodyData);
+  } catch (error) {
+    throw new Error('noon_gate_producer_internal_error');
+  }
+  return response;
+};
+
+exports.sendRecordToNoonGate = sendRecordToNoonGate;
